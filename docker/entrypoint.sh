3489732751a8b95b6637c3b18dd8e1755c87afee
Anthony Loredo <anthonyloredo@Anthonys-MacBook-Pro.local>
2024-02-07 18:47:22 -0800

    changed start process to run on port 80.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@
 ./wait-for-postgres.sh "$DBHOST"
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:80
